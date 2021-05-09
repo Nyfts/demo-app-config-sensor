0982518ba687cd0f4d2a97636dd9f15e940e6a02
Luan Jesus <luan.j0612@gmail.com>
2021-05-09 11:48:49 -0300

    adicionado formulario

---
+++ src/components/TextInput/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from "react";
+import ReactInputMask from 'react-input-mask';
+import { useField } from "@unform/core";
+
+import './index.css';
+
+export default function InputText({ name, isLogin, icon, label, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+
+  const mystyle = {
+    flex: 1,
+    borderLeft: "none",
+    borderRadius: 0,
+    fontSize: "14px",
+    padding: "13px",
+    border: '1px solid #b0b0b0',
+    borderRadius: '5px'
+  };
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+      
+      setValue(ref, value) {
+        // ref.setInputValue(value);
+        ref.value = value;
+      },
+      clearValue(ref) {
+        ref.value = "";
+      },
+
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <div className="text-input-wrapper">
+      <span className="text-input-label">{label}</span>
+      <div className="text-input-container">
+        <ReactInputMask maskPlaceholder={null} className="masked-text-input" style={mystyle} ref={inputRef} defaultValue={defaultValue} {...rest} />
+      </div>
+    </div>
+  );
+}
--- src/pages/FormPage/index.js
+++ src/pages/FormPage/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { Form } from "@unform/web";
 
-import './index.css';
+import "./index.css";
+
+import TextInput from "../../components/TextInput";
 
 function FormPage() {
-  return <div>FormPage</div>;
+  const formRef = useRef();
+
+  const handleFormSubmit = (data) => {
+    console.log(data);
+  };
+
+  const handleReturn = () => {
+    console.log('return');
+  }
+
+  return (
+    <Form ref={formRef} onSubmit={handleFormSubmit}>
+      <TextInput name="username" label="Username" />
+      <TextInput name="password" label="password" />
+      <div className="submit-button-wrapper">
+        <button className="submit-button return-button">Voltar</button>
+        <button className="submit-button" type="submit">
+          Enviar
+        </button>
+      </div>
+    </Form>
+  );
 }
 
 export default FormPage;
